35c700a7c1a78132aad11dfcfd6f4252d4b4e12e
Lysenko <lysenkoden@gmail.com>
2025-01-28 12:43:34 +0100

    added tests for statuses in useData hook

--- src/__tests__/hooks.spec.js
+++ src/__tests__/hooks.spec.js
@@ -1,5 +1,12 @@
-import { renderHook, act } from '@testing-library/react';
-import { useBooleanToggle } from '../hooks';
+import { renderHook, act, waitFor } from '@testing-library/react';
+import { useBooleanToggle, useData } from '../hooks';
+import { open, getItems, addItem } from '../utils/indexdb';
+
+jest.mock('../utils/indexdb', () => ({
+  getItems: jest.fn(),
+  addItem: jest.fn(),
+  open: jest.fn(),
+}));
 
 describe('useBooleanToggle hook', () => {
   it('should handle toggle', () => {
@@ -9,3 +16,39 @@ describe('useBooleanToggle hook', () => {
     expect(result.current.status).toBe(true);
   });
 });
+
+describe('useData hook', () => {
+  beforeEach(() => {
+    open.mockImplementation(() => Promise.resolve()); // Mock open to return a resolved Promise
+    getItems.mockImplementation(() => Promise.resolve([{ value: 1 }]));
+  });
+
+  it('should get empty transactions', () => {
+    const { result } = renderHook(() => useData());
+    expect(result.current.transactions).toEqual([]);
+  });
+
+  it('should set status to PENDING', () => {
+    const { result } = renderHook(() => useData());
+    expect(result.current.status).toBe('PENDING');
+  });
+
+  it('should set mocked transactions', async () => {
+    const { result } = renderHook(() => useData());
+    await waitFor(() => {
+      expect(result.current.transactions).toEqual([{ value: 1 }]);
+    });
+  });
+
+  it('should set SUCCESS', async () => {
+    const { result } = renderHook(() => useData());
+
+    await waitFor(() => {
+      expect(result.current.status).toBe('SUCCESS');
+    });
+  });
+});
+
+//test should set REJECT
+//test onDelete
+//test onStarClick
