e00ca00826fa5371727a5d5f38a48982a013388c
Lysenko <lysenkoden@gmail.com>
2025-02-01 09:10:20 +0100

    fixed negative value transaction background

--- src/app/page.js
+++ src/app/page.js
@@ -30,7 +30,7 @@ const Home = () => {
       {loading ? (
         <div>Loading...</div>
       ) : (
-        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-2 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
           <main className="flex flex-col gap-4 row-start-2 items-center sm:items-start">
             <Heading />
             <ErrorBoundary fallback={<p>Something went wrong...</p>}>
--- src/components/Transaction/index.js
+++ src/components/Transaction/index.js
@@ -9,7 +9,7 @@ const Transaction = ({ transaction, onDelete, onStarClick }) => {
   const bgColor =
     value >= 0
       ? `py-2 px-5 me-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 shadow-lg flex w-full justify-between`
-      : `py-2 px-5 me-2 text-sm font-medium text-red-900 focus:outline-none bg-red rounded-lg border border-red-200 hover:bg-red-100 hover:text-red-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-red-800 dark:text-red-400 dark:border-red-600 dark:hover:text-white dark:hover:bg-red-700 shadow-lg bg-red-200 flex w-full justify-between`;
+      : `py-2 px-5 me-2 text-sm font-medium text-red-900 focus:outline-none bg-red rounded-lg border border-red-200 hover:bg-red-100 hover:text-red-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-red-300 dark:text-red-900 dark:border-red-600 dark:hover:text-white dark:hover:bg-red-700 shadow-lg bg-red-200 flex w-full justify-between`;
 
   const { state } = useContext(AppContext);
 
