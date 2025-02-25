c2d14a7a7138da954277fd02745b06b2453205af
Lysenko <lysenkoden@gmail.com>
2025-02-25 23:19:58 +0100

    aligned transaction

--- README.md
+++ README.md
@@ -99,5 +99,6 @@ https://budget4u.vercel.app
 <li>❎ Set your cashflow! - make visible in dark mode </li>
 <li>❎ language and currency - to the localstore </li>
 <li>❎ add edit button --> make it icon not a word </li>
-
+<li>❎ align transaction with sections grid</li>
+<li>❎ align nav with grid for mobile devices</li>
 </ul>
--- src/components/Transaction/index.js
+++ src/components/Transaction/index.js
@@ -9,8 +9,8 @@ const Transaction = memo(({ transaction, onDelete, onStarClick }) => {
   const { id, value, date, comment, isStarred } = transaction;
   const bgColor =
     value >= 0
-      ? `flex justify-between gap-1 py-2 px-5 me-2 text-[0.5rem] sm:text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 shadow-lg flex w-full justify-between items-center`
-      : `flex justify-between gap-1 py-2 px-5 me-2 text-[0.5rem] sm:text-sm font-medium text-red-900 focus:outline-none bg-red rounded-lg border border-red-200 hover:bg-red-100 hover:text-red-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-red-300 dark:text-red-900 dark:border-red-600 dark:hover:text-white dark:hover:bg-red-700 shadow-lg bg-red-200 flex w-full justify-between items-center`;
+      ? `grid grid-cols-6 items-center py-2 px-4 sm:px-6 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 shadow-lg hover:bg-gray-100 hover:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700`
+      : `grid grid-cols-6 items-center py-2 px-4 sm:px-6 text-sm font-medium text-red-900 bg-red-200 rounded-lg border border-red-200 shadow-lg hover:bg-red-100 hover:text-red-700 dark:bg-red-300 dark:text-red-900 dark:border-red-600 dark:hover:text-white dark:hover:bg-red-700`;
 
   const { state } = useContext(AppContext);
 
@@ -18,27 +18,31 @@ const Transaction = memo(({ transaction, onDelete, onStarClick }) => {
 
   return (
     <div className={bgColor}>
-      <Image
-        onClick={() => onStarClick(id)}
-        src={isStarred ? StarFilled : Star}
-        alt="Unfilled Star"
-        width={20}
-        height={20}
-        className="w-5 h-5 cursor-pointer mx-1 dark:shadow-white shadow-sm rounded-full"
-      />
-      <p>
-        {value.toFixed(2)}
-        {/* {value.toFixed(2)}, {state.currency} */}
+      {/* Star Icon */}
+      <div className="text-center max-w-[20px]">
+        <Image
+          onClick={() => onStarClick(id)}
+          src={isStarred ? StarFilled : Star}
+          alt="Star"
+          width={16}
+          height={16}
+          className="w-5 h-5 cursor-pointer mx-auto dark:shadow-white shadow-sm rounded-full"
+        />
+      </div>
+      <p className="text-right min-w-[60px]">{value.toFixed(2)}</p>
+      <p className="text-center min-w-[60px] sm:min-w-[80px]">{date}</p>
+      <p className="text-center min-w-[80px] sm:min-w-[100px]">Category</p>
+      <p className="text-center min-w-[120px] sm:min-w-[150px] truncate">
+        {comment}
       </p>
-      <p>{date}</p>
-      <p>category</p>
-      <p>{comment}</p>
-      <button
-        onClick={deleteItem}
-        className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-1 px-2 mt-1 border border-blue-500 hover:border-transparent rounded"
-      >
-        <FormattedMessage id="transaction.delete" />
-      </button>
+      <div className="flex justify-center">
+        <button
+          onClick={deleteItem}
+          className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-1 px-2 border border-blue-500 hover:border-transparent rounded"
+        >
+          <FormattedMessage id="transaction.delete" />
+        </button>
+      </div>
     </div>
   );
 });
