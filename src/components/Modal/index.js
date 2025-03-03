98de7addbaee75a237941ed4ffebc6a9862399fa
Lysenko <lysenkoden@gmail.com>
2025-03-03 10:11:44 +0100

    moved up the + button

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -24,7 +24,7 @@ const Modal = ({ onSave, editData, onEdit, isOpen, onOpen, onClose }) => {
         <button
           onClick={onOpen}
           className="w-10 h-10 bg-gradient-to-r from-blue-500 to-indigo-500 text-white 
-          rounded-full fixed bottom-8 right-8 text-3xl shadow-xl flex items-center justify-center 
+          rounded-full fixed top-16 right-8 text-3xl shadow-xl flex items-center justify-center 
           transition-all duration-300 transform hover:scale-110 hover:shadow-2xl 
           active:scale-95 active:shadow-md dark:opacity-70 z-10"
         >
