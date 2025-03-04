bee28786aee0adc900fd5bc1e78a5bf7a7bedd72
Lysenko <lysenkoden@gmail.com>
2025-03-04 10:21:44 +0100

    fixed add first transaction - temporary - need to make one hook

--- src/components/ChangeBalance/index.js
+++ src/components/ChangeBalance/index.js
@@ -1,5 +1,28 @@
+import { useState } from 'react';
 import Modal from '../Modal';
 
-const ChangeBalance = ({ onSave }) => <Modal onSave={onSave} />;
+const ChangeBalance = ({ onSave }) => {
+  const [isModalOpen, setModalOpen] = useState(false); // Стан для відкриття/закриття модального вікна
+
+  const handleOpenModal = () => {
+    setModalOpen(true); // Відкриває модальне вікно
+  };
+
+  const handleCloseModal = () => {
+    setModalOpen(false); // Закриває модальне вікно
+  };
+
+  return (
+    <div>
+      {/* Modal компонент з передачею необхідних пропсів */}
+      <Modal
+        onOpen={handleOpenModal}
+        isOpen={isModalOpen}
+        onClose={handleCloseModal}
+        onSave={onSave}
+      />
+    </div>
+  );
+};
 
 export default ChangeBalance;
