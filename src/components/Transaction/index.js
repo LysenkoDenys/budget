18b5ed20c6285823f55c088a8befbd0eee215551
Lysenko <lysenkoden@gmail.com>
2025-03-28 12:20:08 +0100

    removed edit button to the onClick on the transaction

--- src/components/Transaction/index.js
+++ src/components/Transaction/index.js
@@ -17,23 +17,23 @@ const Transaction = memo(({ transaction, onDelete, onStarClick, onEdit }) => {
 
   const bgColor =
     value >= 0
-      ? `grid items-center grid-cols-[20px_50px_60px_50px_auto_50px] 
-       xs:grid-cols-[20px_70px_80px_50px_auto_50px] 
-       sm:grid-cols-[30px_100px_140px_120px_auto_60px] 
-       md:grid-cols-[30px_100px_180px_150px_auto_70px] 
-       lg:grid-cols-[30px_100px_220px_180px_auto_80px] 
-       xl:grid-cols-[30px_120px_250px_200px_auto_100px] py-3 px-2 
+      ? `grid items-center grid-cols-[20px_50px_60px_50px_auto_25px] 
+       xs:grid-cols-[20px_70px_80px_50px_auto_20px] 
+       sm:grid-cols-[30px_100px_140px_120px_auto_25px] 
+       md:grid-cols-[30px_100px_180px_150px_auto_25px] 
+       lg:grid-cols-[30px_100px_220px_180px_auto_25px] 
+       xl:grid-cols-[30px_120px_250px_200px_auto_25px] py-3 px-2 
        text-green-900 bg-green-200 rounded-lg border border-green-200 shadow-lg 
        hover:bg-green-100 hover:text-green-700 
        dark:bg-green-900 dark:text-green-200 dark:border-green-600 
-       dark:hover:text-white dark:hover:bg-green-800`
-      : `grid items-center grid-cols-[20px_50px_60px_50px_auto_50px] 
-       xs:grid-cols-[20px_70px_80px_50px_auto_50px] 
-       sm:grid-cols-[30px_100px_140px_120px_auto_60px] 
-       md:grid-cols-[30px_100px_180px_150px_auto_70px] 
-       lg:grid-cols-[30px_100px_220px_180px_auto_80px] 
-       xl:grid-cols-[30px_120px_250px_200px_auto_100px] 
-      py-3 px-2 bg-white rounded-lg border border-gray-200 shadow-lg hover:bg-gray-100 hover:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700`;
+       dark:hover:text-white dark:hover:bg-green-800 cursor-pointer`
+      : `grid items-center grid-cols-[20px_50px_60px_50px_auto_30px] 
+       xs:grid-cols-[20px_70px_80px_50px_auto_25px] 
+       sm:grid-cols-[30px_100px_140px_120px_auto_25px] 
+       md:grid-cols-[30px_100px_180px_150px_auto_25px] 
+       lg:grid-cols-[30px_100px_220px_180px_auto_25px] 
+       xl:grid-cols-[30px_120px_250px_200px_auto_25px] 
+      py-3 px-2 bg-white rounded-lg border border-gray-200 shadow-lg hover:bg-gray-100 hover:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 cursor-pointer`;
 
   const { state } = useContext(AppContext);
 
@@ -44,7 +44,7 @@ const Transaction = memo(({ transaction, onDelete, onStarClick, onEdit }) => {
   );
 
   return (
-    <div className={bgColor}>
+    <div className={bgColor} onClick={editItem}>
       <div className="flex justify-center max-w-[20px]">
         <Image
           onClick={() => onStarClick(id)}
@@ -68,16 +68,10 @@ const Transaction = memo(({ transaction, onDelete, onStarClick, onEdit }) => {
       <p className="text-left truncate min-w-0 sm:text-sm text-[0.7rem]">
         {comment}
       </p>
-      <div className="flex justify-center">
-        <RiEditBoxLine
-          className="text-2xl mr-1 sm:mr-3 text-blue-500 cursor-pointer hover:text-blue-700"
-          onClick={editItem}
-        />
-        <RiDeleteBin4Line
-          onClick={() => setIsModalOpen(true)}
-          className="text-2xl cursor-pointer text-red-500 hover:text-red-700 dark:text-red-400"
-        />
-      </div>
+      <RiDeleteBin4Line
+        onClick={() => setIsModalOpen(true)}
+        className="text-xl cursor-pointer text-red-500 hover:text-red-700 dark:text-red-400"
+      />
       <ConfirmModal
         isOpen={isModalOpen}
         onClose={() => setIsModalOpen(false)}
