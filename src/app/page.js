35c141a906c39a063b8713a2bb1dcae63db91ff6
Lysenko <lysenkoden@gmail.com>
2025-03-29 08:53:22 +0100

    centered data in Home page

--- src/app/page.js
+++ src/app/page.js
@@ -69,7 +69,7 @@ const Home = () => {
         <Spinner />
       ) : (
         <div className="grid grid-rows-[10px_auto_10px] items-center justify-items-center h-full p-2 pb-2 pt-10 font-[family-name:var(--font-geist-sans)]">
-          <div className="sticky top-[40px] z-40 w-full">
+          <div className="sticky top-[40px] z-40 w-full flex flex-col justify-center items-center">
             <BalanceData transactions={transactions}>
               {(balance) => <Balance balance={balance} />}
             </BalanceData>
@@ -91,7 +91,9 @@ const Home = () => {
             )}
             {/* Handle empty transactions */}
             {status === STATUSES.SUCCESS && transactions.length === 0 && (
-              <div>No transactions available</div>
+              <div className="flex items-center justify-center h-full w-full text-center">
+                No transactions available
+              </div>
             )}
           </main>
           <footer className="row-start-3 flex gap-0 flex-wrap items-center justify-center"></footer>
