7d41e918a3606b7c5eff4efdf804e88f3e060c44
Lysenko <lysenkoden@gmail.com>
2025-04-06 07:40:01 +0200

    fixed formatNumber 16 400 --> 16.4K

--- src/utils/formatNumber.js
+++ src/utils/formatNumber.js
@@ -1,25 +1,25 @@
 const formatNumber = (number, showDecimals = false, locale = 'en-US') => {
-  const format = (n) => {
-    return new Intl.NumberFormat(locale, {
-      minimumFractionDigits: showDecimals ? 1 : 0,
-      maximumFractionDigits: showDecimals ? 1 : 0,
+  const format = (n, decimals) => {
+    const formatted = new Intl.NumberFormat(locale, {
+      minimumFractionDigits: decimals,
+      maximumFractionDigits: decimals,
     }).format(n);
+
+    // Remove trailing zeros and dot/comma if needed
+    return formatted.replace(/([.,]0+)$|([.,]\d*[1-9])0+$/, '$2');
   };
 
   if (Math.abs(number) >= 1.0e9) {
-    return format(number / 1.0e9) + 'B';
+    return format(number / 1.0e9, 2) + 'B';
   }
   if (Math.abs(number) >= 1.0e6) {
-    return format(number / 1.0e6) + 'M';
+    return format(number / 1.0e6, 2) + 'M';
   }
   if (Math.abs(number) >= 1.0e3) {
-    return format(number / 1.0e3) + 'K';
+    return format(number / 1.0e3, 2) + 'K';
   }
 
-  return new Intl.NumberFormat(locale, {
-    minimumFractionDigits: showDecimals ? 2 : 0,
-    maximumFractionDigits: showDecimals ? 2 : 0,
-  }).format(number);
+  return format(number, showDecimals ? 2 : 0);
 };
 
 export default formatNumber;
